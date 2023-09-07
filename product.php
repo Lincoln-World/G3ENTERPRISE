742e52a8da682518aa2e9ca6011eee704cf111fb
Lincoln-World <ogwucheabraham08@gmail.com>
2023-09-07 17:12:11 +0100

    added bootstrap

--- product.php
+++ product.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>product</title>
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
 </head>
 <body>
-    
+    <div>
+        <h1>product</h1>
+    </div>
 </body>
 </html>
